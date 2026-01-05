c1e58d5b37adfc99fdb6e61351c341f203ba9b20
Sunny Raj <153062846+rajSunny5714@users.noreply.github.com>
2026-01-05 10:29:44 +0530

    Add files via upload

---
+++ Java/NestedPattern/ZeroOnePattern.java
@@ -0,0 +1,15 @@
+package NestedPattern;
+import java.util.*;
+
+public class ZeroOnePattern {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print((i+j)%2+" ");
+            }
+            System.out.println();
+        }
+    }
+}
